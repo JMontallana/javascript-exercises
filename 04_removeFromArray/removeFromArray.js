62fa9517cb2891dfb2c4e6b93f1a5eca7a42030f
JMontallana <jeffrymontallana28@gmail.com>
2023-02-11 01:38:15 +0800

    added exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    // const newArr = [];
+    // arr.forEach(element => {
+    //     if (!args.includes(element)) {
+    //         newArr.push(element);
+    //         console.log(newArr)
+    //     }
+    // });
+    // return newArr
 
-};
+    const newArray = [];
+    for(let i = 0; i < arr.length; i++ ){
+        if(!args.includes(arr[i])){
+            newArray.push(arr[i]);
+            console.log(newArray);
+        } 
+    }
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
