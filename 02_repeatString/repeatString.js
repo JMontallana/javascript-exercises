807331a146fa3000c3bef3d44b8d421675fa8395
JMontallana <jeffrymontallana28@gmail.com>
2023-02-11 00:22:07 +0800

    done with exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
 const repeatString = function(putString, putNumber) {
-
+    if(putNumber < 0) {
+        return 'ERROR';
+    } else if (putNumber >= 0) {
+        let string = '';
+        for(let i = 0; i < putNumber; i++){
+            string += putString;
+        }
+        return string;
+    } 
 };
 
 // Do not edit below this line
