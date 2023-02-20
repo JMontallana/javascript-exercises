e2fe1e3b6e7873e12750738e3af1fb5c916d7298
JMontallana <jeffrymontallana28@gmail.com>
2023-02-20 22:46:07 +0800

    added palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let re = /[^a-zA-Z0-9]/g; //Regex to remove unwanted characters
 
+    let lowStr = str
+                    .toLowerCase()
+                    .replace(re, '');
+
+    const reverseStr = lowStr
+                            .split('')
+                            .reverse()
+                            .join('');
+
+    if(reverseStr === lowStr) {
+        return true;
+    }  else {
+        return false;
+    }          
 };
 
 // Do not edit below this line
