92576a14b33c611cb7ef75c253d8d8b7178c9de3
JMontallana <jeffrymontallana28@gmail.com>
2023-02-20 23:43:36 +0800

    added fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    
+    if(Number(number) <= 1 && Number(number) >=0) return Number(number);
+    if (number < 0) return "OOPS";
 
+    const result = [0, 1];
+
+    for (let i = 2; i <= Number(number); i++) {
+        result[i] = result[i - 2] + result[i - 1];
+    }
+
+    return result[result.length - 1];
 };
 
 // Do not edit below this line
