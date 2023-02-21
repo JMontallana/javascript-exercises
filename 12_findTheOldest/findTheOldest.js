e7861738607a7c4cf0941ddcae4c936ddc8e000d
JMontallana <jeffrymontallana28@gmail.com>
2023-02-21 23:03:18 +0800

    find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    
+    return array.reduce((previous, current) => {
+        const previousAge = getAge(previous.yearOfBirth, previous.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return previousAge < currentAge ? current : previous;
+    }) 
 
+    
+};
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
